e02404c30ea3970896b12459d5ae231b7af55af4
priven-m <pmnisi.intecon.co.za>
2022-02-15 16:26:00 +0200

    java class 2

---
+++ firstpkg/firstClass.java
@@ -0,0 +1,10 @@
+package firstpkg;
+
+public class firstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey, This is my first Class");
+	}
+
+}

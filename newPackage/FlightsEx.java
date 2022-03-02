90f27ae5d2d1e983c14091728e04a3b8a6cf1207
priven-m <pmnisi.intecon.co.za>
2022-03-02 07:34:19 +0200

    01032022 commit

--- newPackage/FlightsEx.java
+++ newPackage/FlightsEx.java
@@ -1,9 +1,12 @@
 package newPackage;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class FlightsEx {
 
@@ -23,8 +26,43 @@ public class FlightsEx {
 		//Click on Flights link
 		driver.findElement(By.cssSelector("a[href='reservation.php']")).click();
 		
-		driver.findElement(By.cssSelector("name['tripType']value['oneway']"));
+		//url
+		driver.get("https://demo.guru99.com/test/newtours/reservation.php");
 		
+		//locate using xpath
+		WebElement oneWayRb = driver.findElement(By.xpath("//input[@type='radio'][@value='oneway']"));
+		
+		oneWayRb.click();
+		System.out.println("One way radio Button clicked");
+		Thread.sleep(100);
+		
+		//Select the dropdown using name
+		Select select = new Select(driver.findElement(By.name("fromPort")));
+		
+		//print options in select dropdown
+		List<WebElement>lst= select.getOptions();
+		
+		//Loopin through options
+		for (WebElement options:lst)
+			System.out.println(options.getText());
+		
+		//select the option
+		select.selectByVisibleText("London");
+		System.out.println(select.getFirstSelectedOption().getText());
+		
+		//locate using xpath
+		WebElement firstClassRb = driver.findElement(By.xpath("//input[@name='servClass'][@value='First']"));
+				
+		firstClassRb.click();
+		System.out.println("First Class radio Button clicked");
+		
+		//Click continue
+		driver.findElement(By.name("findFlights")).click();
+		System.out.println("Form submitted");
+		
+		driver.close();
+		              
+
 	}
 
 }
